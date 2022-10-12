56844e19a05e7cb3981a37052950eee5d00f836c
Aidan Underwood <aidanunderwood1@icloud.com>
2022-10-12 17:23:43 -0500

    print hello

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -92,6 +92,7 @@ public class Robot extends TimedRobot {
     public void testInit() {
         // Cancels all running commands at the start of test mode.
         CommandScheduler.getInstance().cancelAll();
+        System.out.println("Hello");
     }
 
     /** This function is called periodically during test mode. */
